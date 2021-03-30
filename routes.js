8bbd5c24b46e975b409f7789ae0520cfca52cc45
Gabriel Ribeiro <gabrielkaina99@gmail.com>
2021-03-30 00:12:41 -0300

    errors messages

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
+const loginController = require('./src/controllers/LoginController');
 
 // Rotas da home
 route.get('/', homeController.index);
 
 // Rotas de login
 route.get('/login/index', loginController.index);
-route.get('/login/register', loginController.register);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
